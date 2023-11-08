15e4e30ce0bbee610c6db723ffcaa605745ba8c8
Ana Ianni <70654287+Ana-Ianni@users.noreply.github.com>
2023-11-08 19:06:04 -0300

    copy all content but header and footer feature

--- content.js
+++ content.js
@@ -1,30 +1,45 @@
-document.getElementById('trigger').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-
-      function getContent() {
-          let content = document.querySelector(".container-fluid");
-          
-          if (!content.innerText) return;
-          content = content.innerText;
-          
-          const textArea = document.createElement("textarea");
-          textArea.value = content;
-        
-          document.body.appendChild(textArea);
-        
-          textArea.select();
-        
-          document.execCommand("copy");
-        
-          document.body.removeChild(textArea);
-
-          alert("Copied to clipboard! :)");
-      };
-
-      chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          func: getContent,
-      });
-  });
+document.getElementById('trigger1').addEventListener('click', () => { 
+    extractContent("#trigger1");
 });
+
+document.getElementById('trigger2').addEventListener('click', () => { 
+    extractContent("#trigger2");
+});
+
+function extractContent(trigger) { 
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+  
+        function getContent(trigger) {
+            let content;
+            
+            if (trigger === "#trigger1") {
+                content = document.querySelector(".container-fluid, .container");
+            } else if (trigger === "#trigger2") {
+                content = document.querySelector("#content, .container.responsivegrid:not(.root)");
+            }
+
+            if (!content.innerText || !content.innerText) return;
+            content = content.innerText;
+            
+            const textArea = document.createElement("textarea");
+            textArea.value = content;
+          
+            document.body.appendChild(textArea);
+          
+            textArea.select();
+          
+            document.execCommand("copy");
+          
+            document.body.removeChild(textArea);
+  
+            alert("Copied to clipboard! :)");
+        };
+  
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: getContent,
+            args: [trigger],
+        });
+    });
+}
